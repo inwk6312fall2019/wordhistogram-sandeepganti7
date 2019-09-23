c214067c65b82cb621acc8650e030b9f8d76381e
dollyanusha <dollyanusha@dal.ca>
2019-09-23 04:59:42 -0700

    part2.py

---
+++ part2.py
@@ -0,0 +1,17 @@
+with open('2600-0.txt') as fd:
+    word_list = fd.read().splitlines()
+
+word_dict = {word: None for word in word_list}
+
+
+def find_rot_pairs():
+    final_list = []
+    for word in word_dict:
+        for i in range(1, 26):
+            if rotate.rotate_word(word, i) in word_dict:
+                final_list.append((word, i, rotate.rotate_word(word, i)))
+    final_list.sort()
+    for pair in final_list:
+        print pair
+
+find_rot_pairs()
